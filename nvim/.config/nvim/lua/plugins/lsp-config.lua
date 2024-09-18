085e9af88e4533f33f34846cc45a0651a57f17d2
Alexander Jansson <alexander.oscar.jansson@gmail.com>
2024-09-18 13:35:45 +0200

    added goto error keybind

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -27,6 +27,7 @@ return {
             vim.keymap.set('n', '<leader>ho', vim.lsp.buf.hover, {})
             vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<leader>ge', vim.diagnostic.goto_next, {})
 
             -- lua setup 
             lspconfig.lua_ls.setup({
